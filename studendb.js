adedef89fd5e4b71f8486550f89b7c4a78555911
JuanSe320 <63017408+JuanSe320@users.noreply.github.com>
2023-08-29 19:29:38 -0500

    Update studendb.js

--- studendb.js
+++ studendb.js
@@ -1,4 +1,5 @@
 const datastudent = {
+"201942104": {"nombre" : "juan sebastian", "semestre": 6, "soltero":true, "gustos" :['comer', 'jugar video juegos', 'ejercitarme']}, 
 "20180605": {"nombre" : "Fredy" , "semestre": 6 , "soltero":false , "gustos" : ['comer', 'bailar','viajar']},
 "1940805-3743":"",
 "2043246-3743":{"nombre" : "José" , "semestre": 6 , "soltero":true , "gustos" : ['videojuegos', 'series','comer']},
